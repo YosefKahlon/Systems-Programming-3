be44507fe8b25c06685e89e750e95327dfa30b43
yosefkahlon <yosefkahlon53@gmail.com>
2021-12-19 12:28:07 +0200

    test

--- str.c
+++ str.c
@@ -347,17 +347,3 @@ void func3(char word[], char txt[]) {
     printf("Anagram Sequences: %s", str);
 }
 
-//int main() {
-//    char word[WORD] ;
-//    char txt[TXT] ;
-//    scanWord(word);
-//    char cpyWord[WORD];
-//    strcpy(cpyWord,word);
-//    scanTxt(txt);
-//    char cpyTxt[TXT];
-//    strcpy(cpyTxt,txt);
-//    func1(word, txt);
-//    func2(word, txt);
-//    func3(cpyWord, txt);
-//    return  0;
-//}
